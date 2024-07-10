25cfb3ed818a2b9f9379e08f7bbfd524b3e650c3
ncb6206 <ncb6206@gmail.com>
2024-07-10 14:49:52 +0900

    Refactor : FreeBoardList컴포넌트를 QuestionBoardList와 코드가 비슷하게 수정

--- src/components/board/FreeBoardList/index.tsx
+++ src/components/board/FreeBoardList/index.tsx
@@ -1,9 +1,10 @@
-import styled from 'styled-components';
-import Profile from '../Profile';
-import TagList from '../TagList';
-import Status from '../Status';
 import BookMarkIcon from '../BookMarkIcon';
-import Block from '../Block';
+import { Board, BoardListWrapper } from './styles';
+import { Filters } from '../../../types/filter';
+import usePosts from '../../../hooks/usePosts';
+import PaginationRounded from '../Pagination';
+import BoardItem from '../QuestionBoardList/BoardItem';
+import usePost from '../../../hooks/usePost';
 // import More from '../More';
 
 // interface Option {
@@ -13,105 +14,78 @@ import Block from '../Block';
 // interface Props {
 //   options: Option[];
 // }
-const list = [
-  {
-    id: 1,
-    nickname: '가나다라마바사',
-    title: '~~인데 맞음?',
-    tags: ['coding', 'react', 'spring', 'html', 'vscode', 'css', 'js', 'study'],
-    viewCount: 20,
-    commentCount: 20,
-    likeCount: 20,
-    bookmark: false,
-    block: false,
-  },
-  {
-    id: 2,
-    nickname: '가나다라마바사',
-    title: '~~인데 맞음?',
-    tags: ['coding', 'react', 'spring', 'html', 'vscode', 'css', 'js', 'study'],
-    viewCount: 20,
-    commentCount: 20,
-    likeCount: 20,
-    bookmark: true,
-    block: true,
-  },
-];
 
-function FreeBoardList() {
-  const handleClick = () => {
-    console.log('블럭처리 작업 ');
-  };
+interface Props {
+  filters: Filters;
+}
+
+function FreeBoardList({ filters }: Props) {
+  const { posts, showLoadingMessage, showNoDataMessage, showPagination, toggleStatus } = usePosts(filters);
+  const { handleBookMarkIconClick, handleMoreClick, handleBlindDataAddClick, handlePostClick, eventStop } = usePost({
+    posts,
+    toggleStatus,
+  });
+
+  if (showLoadingMessage) {
+    return (
+      <BoardListWrapper>
+        <Board>데이터 로딩중...</Board>
+      </BoardListWrapper>
+    );
+  }
+
+  if (showNoDataMessage) {
+    return (
+      <BoardListWrapper>
+        <Board>데이터 0건</Board>
+      </BoardListWrapper>
+    );
+  }
 
   return (
-    <Container>
-      {list.map(({ id, nickname, title, tags, viewCount, commentCount, likeCount, bookmark, block }) => (
-        <BoardItem key={id}>
-          {block && <Block onClick={handleClick} id={id} />}
-          <Profile avatar="" nickname={nickname} timeline="" />
-          <div className="title">
-            <h1>{title}</h1>
-            <TagList tags={tags} />
-          </div>
-          <div className="timestamp">1시간 전</div>
-
-          <div className="status">
-            <Status viewCount={viewCount} commentCount={commentCount} likeCount={likeCount} />
-          </div>
-
-          <BookMarkIcon isActive={bookmark} />
-
-          {!block && (
-            <MoreWrapper>
-              {/* <More options={[{ label: '이 글 더 이상 그만보기', callback: () => {} }]} /> */}
-            </MoreWrapper>
-          )}
-        </BoardItem>
+    <BoardListWrapper>
+      {posts.map((post) => (
+        <BoardItem
+          key={post.id}
+          post={post}
+          handlePostClick={handlePostClick}
+          handleBookMarkIconClick={handleBookMarkIconClick}
+          handleMoreClick={handleMoreClick}
+          handleBlindDataAddClick={handleBlindDataAddClick}
+          eventStop={eventStop}
+          BookMarkIconMemo={BookMarkIcon}
+        />
       ))}
-    </Container>
+      {showPagination && <PaginationRounded />}
+    </BoardListWrapper>
   );
+  // return (
+  //   <Container>
+  //     {list.map(({ id, nickname, title, tags, viewCount, commentCount, likeCount, bookmark, block }) => (
+  //       <BoardItem key={id}>
+  //         {block && <Block onClick={handleClick} id={id} />}
+  //         <Profile avatar="" nickname={nickname} timeline="" />
+  //         <div className="title">
+  //           <h1>{title}</h1>
+  //           <TagList tags={tags} />
+  //         </div>
+  //         <div className="timestamp">1시간 전</div>
+
+  //         <div className="status">
+  //           <Status viewCount={viewCount} commentCount={commentCount} likeCount={likeCount} />
+  //         </div>
+
+  //         <BookMarkIcon isActive={bookmark} />
+
+  //         {!block && (
+  //           <MoreWrapper>
+  //             {/* <More options={[{ label: '이 글 더 이상 그만보기', callback: () => {} }]} /> */}
+  //           </MoreWrapper>
+  //         )}
+  //       </BoardItem>
+  //     ))}
+  //   </Container>
+  // );
 }
 
 export default FreeBoardList;
-
-const Container = styled.ul`
-  border-top: 1px solid #ccc;
-  border-bottom: 1px solid #ccc;
-`;
-
-const BoardItem = styled.li`
-  padding: 25px 10px;
-  border-left: 1px solid #ccc;
-  border-right: 1px solid #ccc8;
-  display: flex;
-  align-items: center;
-  position: relative;
-
-  & + li {
-    border-top: 1px solid #5d5a88;
-  }
-
-  .title {
-    margin-left: 50px;
-    width: 550px;
-
-    h1 {
-      font-size: 18px;
-      font-weight: 700;
-    }
-  }
-
-  .timestamp {
-    width: 250px;
-  }
-
-  .status {
-    margin-right: 60px;
-  }
-`;
-
-const MoreWrapper = styled.div`
-  position: absolute;
-  right: -30px;
-  top: 0px;
-`;
