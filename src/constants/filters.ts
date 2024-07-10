2588ca7ec087e4d4ced2f5204cc21e30088cd844
ncb6206 <ncb6206@gmail.com>
2024-07-10 14:49:05 +0900

    Refactor : constants값 추가

--- src/constants/filters.ts
+++ src/constants/filters.ts
@@ -1,14 +1,35 @@
-export const FILTERS_LIST = {
+export const FILTERS_CATEGORIES_LIST = {
+  QUESTION: 'question',
+  FREE: 'free',
+} as const;
+
+export const FILTERS_SORT_LIST = {
+  LATEST: 'latest',
+  OLDEST: 'oldest',
+} as const;
+
+export const FILTERS_POST_LIST = {
   WAIT: 'wait',
   FINISH: 'finish',
   ALL: '',
 } as const;
 
-export const INITIAL_FILTERS = {
+export const INITIAL_QUESTION_FILTERS = {
   search: '',
-  categories: 'question',
-  sortBy: 'latest',
-  status: FILTERS_LIST.ALL,
+  categories: FILTERS_CATEGORIES_LIST.QUESTION,
+  sortBy: FILTERS_SORT_LIST.LATEST,
+  status: FILTERS_POST_LIST.ALL,
+  pageInfo: {
+    page: 0,
+    size: 15,
+  },
+} as const;
+
+export const INITIAL_FREE_FILTERS = {
+  search: '',
+  categories: FILTERS_CATEGORIES_LIST.FREE,
+  sortBy: FILTERS_SORT_LIST.LATEST,
+  status: FILTERS_POST_LIST.ALL,
   pageInfo: {
     page: 0,
     size: 15,
