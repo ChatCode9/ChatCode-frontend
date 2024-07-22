e9fc2b08027bb6e4862b0efa101ca9993d6f5680
ncb6206 <ncb6206@gmail.com>
2024-07-22 15:38:32 +0900

    Type : 타입 세부 사항 수정

--- src/types/avatar.ts
+++ src/types/avatar.ts
@@ -1,13 +1,26 @@
-export interface Avatar {
+export interface AvatarName {
+  name?: string;
+}
+
+export interface Avatars {
   id: number;
   activityPoint: number;
-  content: string;
   nickname: string;
   picture: string;
+}
+
+export interface Avatar extends Avatars {
+  content: string;
 }
 
 export interface AvatarData {
   code: number;
   data: Avatar;
+  message: string;
+}
+
+export interface AvatarsData {
+  code: number;
+  data: Avatars[];
   message: string;
 }
