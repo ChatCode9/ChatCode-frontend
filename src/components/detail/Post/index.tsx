3379a0fd62818004f316791f84e0a9959895dab2
ncb6206 <ncb6206@gmail.com>
2024-07-22 15:52:07 +0900

    Refactor : data를 상속시켜주어 그 컴포넌트 안에서 구조분해할당

--- src/components/detail/Post/index.tsx
+++ src/components/detail/Post/index.tsx
@@ -4,93 +4,33 @@ import { Container } from './styles';
 import PostHeader from '../PostHeader';
 import WriterProfile from '../WriterProfile';
 import { usePostQuery } from '../../../hooks/api/usePostQuery.ts';
-import { useAvatarQuery } from '../../../hooks/api/useAvatarQuery.ts';
+import { spliceTag } from '../../../utils/spliceTag.ts';
 
 interface Props {
   postId: number;
 }
 
 function Post({ postId }: Props) {
-  // const contentRef = useRef<HTMLDivElement>(null);
-  const { postData, isLoadingPost, isErrorPost } = usePostQuery({ postId });
-  const { avatarData, isLoadingAvatar, isErrorAvatar } = useAvatarQuery({ postId });
-
-  const isLoading = isLoadingPost || isLoadingAvatar;
-  const isError = isErrorPost || isErrorAvatar;
-  const isData = !!postData?.data && !!avatarData?.data;
-
-  // useEffect(() => {
-  //   if (contentRef.current && postData?.data) {
-  //     contentRef.current.innerHTML = postData?.data?.content;
-  //     const images = contentRef.current.getElementsByTagName('img');
-  //     // for (const img of images) {
-  //     //   img.src = img.src; // force reload the image
-  //     // }
-  //     console.log(images);
-  //   }
-  // }, [postData]);
+  const { postData } = usePostQuery({ postId });
 
   console.log(postData, postData?.data);
-  console.log(avatarData);
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
-  if (isError) {
-    return <div>Error...</div>;
-  }
-
-  if (isData) {
-    const {
-      title,
-      timeline,
-      viewCount,
-      status,
-      bookmark,
-      tags,
-      content: postContent,
-      likeCount,
-      isLiked,
-    } = postData?.data;
-
-    const { id, nickname, activityPoint, picture, content: avatarContent } = avatarData?.data;
-
-    return (
-      <Container>
-        <PostHeader
-          postId={postId}
-          title={title}
-          timeline={timeline}
-          viewCount={viewCount}
-          status={status}
-          bookmark={bookmark}
-          likeCount={likeCount}
-          isLiked={isLiked}
-        />
-        <div className="tags">
-          {tags.map((tag: string) => (
-            <li key={tag}>#{tag}</li>
-          ))}
-        </div>
-        {/* <div ref={contentRef} /> */}
+  return (
+    <Container>
+      <PostHeader postData={postData?.data} />
+      <div className="tags">
+        {postData?.data.tags && spliceTag(postData?.data.tags).map((tag: string) => <li key={tag}>#{tag}</li>)}
+      </div>
+      {postData?.data && (
         <div
           dangerouslySetInnerHTML={{
-            __html: postContent,
+            __html: postData?.data.content,
           }}
         />
-        <WriterProfile
-          id={id}
-          nickname={nickname}
-          avatar={picture}
-          activityPoint={activityPoint}
-          content={avatarContent}
-        />
-      </Container>
-    );
-  }
-
-  return null;
+      )}
+      <WriterProfile name={postData?.data.nickname} />
+    </Container>
+  );
 }
 
 export default Post;
